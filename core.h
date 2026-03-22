5920b54f9f069a5145a1642f6fad9ad0074f2b7e
Stanley Fuller <stanthesoupking@gmail.com>
2026-03-22 22:16:44 +1100

    Splat, min, max, eq functions

--- core.h
+++ core.h
@@ -268,6 +268,67 @@ typedef sl_aligned_struct(32) mat4x4_f64 {
 	.w = { 0, 0, 0, 1 },\
 })
 
+sl_inline vec2_u8 splat_vec2_u8(u8 v) {
+	return (vec2_u8) {
+		.x = v,
+		.y = v,
+	};
+}
+sl_inline vec2_u16 splat_vec2_u16(u16 v) {
+	return (vec2_u16) {
+		.x = v,
+		.y = v,
+	};
+}
+sl_inline vec2_u32 splat_vec2_u32(u32 v) {
+	return (vec2_u32) {
+		.x = v,
+		.y = v,
+	};
+}
+sl_inline vec2_u64 splat_vec2_u64(u64 v) {
+	return (vec2_u64) {
+		.x = v,
+		.y = v,
+	};
+}
+sl_inline vec2_s8 splat_vec2_s8(s8 v) {
+	return (vec2_s8) {
+		.x = v,
+		.y = v,
+	};
+}
+sl_inline vec2_s16 splat_vec2_s16(s16 v) {
+	return (vec2_s16) {
+		.x = v,
+		.y = v,
+	};
+}
+sl_inline vec2_s32 splat_vec2_s32(s32 v) {
+	return (vec2_s32) {
+		.x = v,
+		.y = v,
+	};
+}
+sl_inline vec2_s64 splat_vec2_s64(s64 v) {
+	return (vec2_s64) {
+		.x = v,
+		.y = v,
+	};
+}
+sl_inline vec2_f32 splat_vec2_f32(f32 v) {
+	return (vec2_f32) {
+		.x = v,
+		.y = v,
+	};
+}
+sl_inline vec2_f64 splat_vec2_f64(f64 v) {
+	return (vec2_f64) {
+		.x = v,
+		.y = v,
+	};
+}
+
 sl_inline vec2_u8 add_vec2_u8(vec2_u8 a, vec2_u8 b) {
 	return (vec2_u8) {
 		.x = a.x + b.x,
@@ -546,6 +607,198 @@ sl_inline vec2_f64 neg_vec2_f64(vec2_f64 a) {
 	return (vec2_f64) {
 		.x = -a.x,
 		.y = -a.y,
+	};
+}
+
+sl_inline vec2_u8 min_vec2_u8(vec2_u8 a, vec2_u8 b) {
+	return (vec2_u8) {
+		.x = sl_min(a.x, b.x),
+		.y = sl_min(a.y, b.y),
+	};
+}
+sl_inline vec2_u16 min_vec2_u16(vec2_u16 a, vec2_u16 b) {
+	return (vec2_u16) {
+		.x = sl_min(a.x, b.x),
+		.y = sl_min(a.y, b.y),
+	};
+}
+sl_inline vec2_u32 min_vec2_u32(vec2_u32 a, vec2_u32 b) {
+	return (vec2_u32) {
+		.x = sl_min(a.x, b.x),
+		.y = sl_min(a.y, b.y),
+	};
+}
+sl_inline vec2_u64 min_vec2_u64(vec2_u64 a, vec2_u64 b) {
+	return (vec2_u64) {
+		.x = sl_min(a.x, b.x),
+		.y = sl_min(a.y, b.y),
+	};
+}
+sl_inline vec2_s8 min_vec2_s8(vec2_s8 a, vec2_s8 b) {
+	return (vec2_s8) {
+		.x = sl_min(a.x, b.x),
+		.y = sl_min(a.y, b.y),
+	};
+}
+sl_inline vec2_s16 min_vec2_s16(vec2_s16 a, vec2_s16 b) {
+	return (vec2_s16) {
+		.x = sl_min(a.x, b.x),
+		.y = sl_min(a.y, b.y),
+	};
+}
+sl_inline vec2_s32 min_vec2_s32(vec2_s32 a, vec2_s32 b) {
+	return (vec2_s32) {
+		.x = sl_min(a.x, b.x),
+		.y = sl_min(a.y, b.y),
+	};
+}
+sl_inline vec2_s64 min_vec2_s64(vec2_s64 a, vec2_s64 b) {
+	return (vec2_s64) {
+		.x = sl_min(a.x, b.x),
+		.y = sl_min(a.y, b.y),
+	};
+}
+sl_inline vec2_f32 min_vec2_f32(vec2_f32 a, vec2_f32 b) {
+	return (vec2_f32) {
+		.x = sl_min(a.x, b.x),
+		.y = sl_min(a.y, b.y),
+	};
+}
+sl_inline vec2_f64 min_vec2_f64(vec2_f64 a, vec2_f64 b) {
+	return (vec2_f64) {
+		.x = sl_min(a.x, b.x),
+		.y = sl_min(a.y, b.y),
+	};
+}
+sl_inline vec2_u8 max_vec2_u8(vec2_u8 a, vec2_u8 b) {
+	return (vec2_u8) {
+		.x = sl_max(a.x, b.x),
+		.y = sl_max(a.y, b.y),
+	};
+}
+sl_inline vec2_u16 max_vec2_u16(vec2_u16 a, vec2_u16 b) {
+	return (vec2_u16) {
+		.x = sl_max(a.x, b.x),
+		.y = sl_max(a.y, b.y),
+	};
+}
+sl_inline vec2_u32 max_vec2_u32(vec2_u32 a, vec2_u32 b) {
+	return (vec2_u32) {
+		.x = sl_max(a.x, b.x),
+		.y = sl_max(a.y, b.y),
+	};
+}
+sl_inline vec2_u64 max_vec2_u64(vec2_u64 a, vec2_u64 b) {
+	return (vec2_u64) {
+		.x = sl_max(a.x, b.x),
+		.y = sl_max(a.y, b.y),
+	};
+}
+sl_inline vec2_s8 max_vec2_s8(vec2_s8 a, vec2_s8 b) {
+	return (vec2_s8) {
+		.x = sl_max(a.x, b.x),
+		.y = sl_max(a.y, b.y),
+	};
+}
+sl_inline vec2_s16 max_vec2_s16(vec2_s16 a, vec2_s16 b) {
+	return (vec2_s16) {
+		.x = sl_max(a.x, b.x),
+		.y = sl_max(a.y, b.y),
+	};
+}
+sl_inline vec2_s32 max_vec2_s32(vec2_s32 a, vec2_s32 b) {
+	return (vec2_s32) {
+		.x = sl_max(a.x, b.x),
+		.y = sl_max(a.y, b.y),
+	};
+}
+sl_inline vec2_s64 max_vec2_s64(vec2_s64 a, vec2_s64 b) {
+	return (vec2_s64) {
+		.x = sl_max(a.x, b.x),
+		.y = sl_max(a.y, b.y),
+	};
+}
+sl_inline vec2_f32 max_vec2_f32(vec2_f32 a, vec2_f32 b) {
+	return (vec2_f32) {
+		.x = sl_max(a.x, b.x),
+		.y = sl_max(a.y, b.y),
+	};
+}
+sl_inline vec2_f64 max_vec2_f64(vec2_f64 a, vec2_f64 b) {
+	return (vec2_f64) {
+		.x = sl_max(a.x, b.x),
+		.y = sl_max(a.y, b.y),
+	};
+}
+
+sl_inline vec3_u8 splat_vec3_u8(u8 v) {
+	return (vec3_u8) {
+		.x = v,
+		.y = v,
+		.z = v,
+	};
+}
+sl_inline vec3_u16 splat_vec3_u16(u16 v) {
+	return (vec3_u16) {
+		.x = v,
+		.y = v,
+		.z = v,
+	};
+}
+sl_inline vec3_u32 splat_vec3_u32(u32 v) {
+	return (vec3_u32) {
+		.x = v,
+		.y = v,
+		.z = v,
+	};
+}
+sl_inline vec3_u64 splat_vec3_u64(u64 v) {
+	return (vec3_u64) {
+		.x = v,
+		.y = v,
+		.z = v,
+	};
+}
+sl_inline vec3_s8 splat_vec3_s8(s8 v) {
+	return (vec3_s8) {
+		.x = v,
+		.y = v,
+		.z = v,
+	};
+}
+sl_inline vec3_s16 splat_vec3_s16(s16 v) {
+	return (vec3_s16) {
+		.x = v,
+		.y = v,
+		.z = v,
+	};
+}
+sl_inline vec3_s32 splat_vec3_s32(s32 v) {
+	return (vec3_s32) {
+		.x = v,
+		.y = v,
+		.z = v,
+	};
+}
+sl_inline vec3_s64 splat_vec3_s64(s64 v) {
+	return (vec3_s64) {
+		.x = v,
+		.y = v,
+		.z = v,
+	};
+}
+sl_inline vec3_f32 splat_vec3_f32(f32 v) {
+	return (vec3_f32) {
+		.x = v,
+		.y = v,
+		.z = v,
+	};
+}
+sl_inline vec3_f64 splat_vec3_f64(f64 v) {
+	return (vec3_f64) {
+		.x = v,
+		.y = v,
+		.z = v,
 	};
 }
 
@@ -873,6 +1126,259 @@ sl_inline vec3_f64 neg_vec3_f64(vec3_f64 a) {
 		.x = -a.x,
 		.y = -a.y,
 		.z = -a.z,
+	};
+}
+
+sl_inline vec3_u8 min_vec3_u8(vec3_u8 a, vec3_u8 b) {
+	return (vec3_u8) {
+		.x = sl_min(a.x, b.x),
+		.y = sl_min(a.y, b.y),
+		.z = sl_min(a.z, b.z),
+	};
+}
+sl_inline vec3_u16 min_vec3_u16(vec3_u16 a, vec3_u16 b) {
+	return (vec3_u16) {
+		.x = sl_min(a.x, b.x),
+		.y = sl_min(a.y, b.y),
+		.z = sl_min(a.z, b.z),
+	};
+}
+sl_inline vec3_u32 min_vec3_u32(vec3_u32 a, vec3_u32 b) {
+	return (vec3_u32) {
+		.x = sl_min(a.x, b.x),
+		.y = sl_min(a.y, b.y),
+		.z = sl_min(a.z, b.z),
+	};
+}
+sl_inline vec3_u64 min_vec3_u64(vec3_u64 a, vec3_u64 b) {
+	return (vec3_u64) {
+		.x = sl_min(a.x, b.x),
+		.y = sl_min(a.y, b.y),
+		.z = sl_min(a.z, b.z),
+	};
+}
+sl_inline vec3_s8 min_vec3_s8(vec3_s8 a, vec3_s8 b) {
+	return (vec3_s8) {
+		.x = sl_min(a.x, b.x),
+		.y = sl_min(a.y, b.y),
+		.z = sl_min(a.z, b.z),
+	};
+}
+sl_inline vec3_s16 min_vec3_s16(vec3_s16 a, vec3_s16 b) {
+	return (vec3_s16) {
+		.x = sl_min(a.x, b.x),
+		.y = sl_min(a.y, b.y),
+		.z = sl_min(a.z, b.z),
+	};
+}
+sl_inline vec3_s32 min_vec3_s32(vec3_s32 a, vec3_s32 b) {
+	return (vec3_s32) {
+		.x = sl_min(a.x, b.x),
+		.y = sl_min(a.y, b.y),
+		.z = sl_min(a.z, b.z),
+	};
+}
+sl_inline vec3_s64 min_vec3_s64(vec3_s64 a, vec3_s64 b) {
+	return (vec3_s64) {
+		.x = sl_min(a.x, b.x),
+		.y = sl_min(a.y, b.y),
+		.z = sl_min(a.z, b.z),
+	};
+}
+sl_inline vec3_f32 min_vec3_f32(vec3_f32 a, vec3_f32 b) {
+	return (vec3_f32) {
+		.x = sl_min(a.x, b.x),
+		.y = sl_min(a.y, b.y),
+		.z = sl_min(a.z, b.z),
+	};
+}
+sl_inline vec3_f64 min_vec3_f64(vec3_f64 a, vec3_f64 b) {
+	return (vec3_f64) {
+		.x = sl_min(a.x, b.x),
+		.y = sl_min(a.y, b.y),
+		.z = sl_min(a.z, b.z),
+	};
+}
+sl_inline vec3_u8 max_vec3_u8(vec3_u8 a, vec3_u8 b) {
+	return (vec3_u8) {
+		.x = sl_max(a.x, b.x),
+		.y = sl_max(a.y, b.y),
+		.z = sl_max(a.z, b.z),
+	};
+}
+sl_inline vec3_u16 max_vec3_u16(vec3_u16 a, vec3_u16 b) {
+	return (vec3_u16) {
+		.x = sl_max(a.x, b.x),
+		.y = sl_max(a.y, b.y),
+		.z = sl_max(a.z, b.z),
+	};
+}
+sl_inline vec3_u32 max_vec3_u32(vec3_u32 a, vec3_u32 b) {
+	return (vec3_u32) {
+		.x = sl_max(a.x, b.x),
+		.y = sl_max(a.y, b.y),
+		.z = sl_max(a.z, b.z),
+	};
+}
+sl_inline vec3_u64 max_vec3_u64(vec3_u64 a, vec3_u64 b) {
+	return (vec3_u64) {
+		.x = sl_max(a.x, b.x),
+		.y = sl_max(a.y, b.y),
+		.z = sl_max(a.z, b.z),
+	};
+}
+sl_inline vec3_s8 max_vec3_s8(vec3_s8 a, vec3_s8 b) {
+	return (vec3_s8) {
+		.x = sl_max(a.x, b.x),
+		.y = sl_max(a.y, b.y),
+		.z = sl_max(a.z, b.z),
+	};
+}
+sl_inline vec3_s16 max_vec3_s16(vec3_s16 a, vec3_s16 b) {
+	return (vec3_s16) {
+		.x = sl_max(a.x, b.x),
+		.y = sl_max(a.y, b.y),
+		.z = sl_max(a.z, b.z),
+	};
+}
+sl_inline vec3_s32 max_vec3_s32(vec3_s32 a, vec3_s32 b) {
+	return (vec3_s32) {
+		.x = sl_max(a.x, b.x),
+		.y = sl_max(a.y, b.y),
+		.z = sl_max(a.z, b.z),
+	};
+}
+sl_inline vec3_s64 max_vec3_s64(vec3_s64 a, vec3_s64 b) {
+	return (vec3_s64) {
+		.x = sl_max(a.x, b.x),
+		.y = sl_max(a.y, b.y),
+		.z = sl_max(a.z, b.z),
+	};
+}
+sl_inline vec3_f32 max_vec3_f32(vec3_f32 a, vec3_f32 b) {
+	return (vec3_f32) {
+		.x = sl_max(a.x, b.x),
+		.y = sl_max(a.y, b.y),
+		.z = sl_max(a.z, b.z),
+	};
+}
+sl_inline vec3_f64 max_vec3_f64(vec3_f64 a, vec3_f64 b) {
+	return (vec3_f64) {
+		.x = sl_max(a.x, b.x),
+		.y = sl_max(a.y, b.y),
+		.z = sl_max(a.z, b.z),
+	};
+}
+
+sl_inline bool eq_vec3_u8(vec4_u8 a, vec4_u8 b) {
+	return (a.x == b.x) && (a.y == b.y) && (a.z == b.z);
+}
+sl_inline bool eq_vec3_u16(vec4_u16 a, vec4_u16 b) {
+	return (a.x == b.x) && (a.y == b.y) && (a.z == b.z);
+}
+sl_inline bool eq_vec3_u32(vec4_u32 a, vec4_u32 b) {
+	return (a.x == b.x) && (a.y == b.y) && (a.z == b.z);
+}
+sl_inline bool eq_vec3_u64(vec3_u64 a, vec3_u64 b) {
+	return (a.x == b.x) && (a.y == b.y) && (a.z == b.z);
+}
+sl_inline bool eq_vec3_s8(vec3_s8 a, vec3_s8 b) {
+	return (a.x == b.x) && (a.y == b.y) && (a.z == b.z);
+}
+sl_inline bool eq_vec3_s16(vec3_s16 a, vec3_s16 b) {
+	return (a.x == b.x) && (a.y == b.y) && (a.z == b.z);
+}
+sl_inline bool eq_vec3_s32(vec3_s32 a, vec3_s32 b) {
+	return (a.x == b.x) && (a.y == b.y) && (a.z == b.z);
+}
+sl_inline bool eq_vec3_s64(vec3_s64 a, vec3_s64 b) {
+	return (a.x == b.x) && (a.y == b.y) && (a.z == b.z);
+}
+sl_inline bool eq_vec3_f32(vec3_f32 a, vec3_f32 b) {
+	return (a.x == b.x) && (a.y == b.y) && (a.z == b.z);
+}
+sl_inline bool eq_vec3_f64(vec3_f64 a, vec3_f64 b) {
+	return (a.x == b.x) && (a.y == b.y) && (a.z == b.z);
+}
+
+sl_inline vec4_u8 splat_vec4_u8(u8 v) {
+	return (vec4_u8) {
+		.x = v,
+		.y = v,
+		.z = v,
+		.w = v,
+	};
+}
+sl_inline vec4_u16 splat_vec4_u16(u16 v) {
+	return (vec4_u16) {
+		.x = v,
+		.y = v,
+		.z = v,
+		.w = v,
+	};
+}
+sl_inline vec4_u32 splat_vec4_u32(u32 v) {
+	return (vec4_u32) {
+		.x = v,
+		.y = v,
+		.z = v,
+		.w = v,
+	};
+}
+sl_inline vec4_u64 splat_vec4_u64(u64 v) {
+	return (vec4_u64) {
+		.x = v,
+		.y = v,
+		.z = v,
+		.w = v,
+	};
+}
+sl_inline vec4_s8 splat_vec4_s8(s8 v) {
+	return (vec4_s8) {
+		.x = v,
+		.y = v,
+		.z = v,
+		.w = v,
+	};
+}
+sl_inline vec4_s16 splat_vec4_s16(s16 v) {
+	return (vec4_s16) {
+		.x = v,
+		.y = v,
+		.z = v,
+		.w = v,
+	};
+}
+sl_inline vec4_s32 splat_vec4_s32(s32 v) {
+	return (vec4_s32) {
+		.x = v,
+		.y = v,
+		.z = v,
+		.w = v,
+	};
+}
+sl_inline vec4_s64 splat_vec4_s64(s64 v) {
+	return (vec4_s64) {
+		.x = v,
+		.y = v,
+		.z = v,
+		.w = v,
+	};
+}
+sl_inline vec4_f32 splat_vec4_f32(f32 v) {
+	return (vec4_f32) {
+		.x = v,
+		.y = v,
+		.z = v,
+		.w = v,
+	};
+}
+sl_inline vec4_f64 splat_vec4_f64(f64 v) {
+	return (vec4_f64) {
+		.x = v,
+		.y = v,
+		.z = v,
+		.w = v,
 	};
 }
 
@@ -1247,6 +1753,37 @@ sl_inline vec4_f64 neg_vec4_f64(vec4_f64 a) {
 		.z = -a.z,
 		.w = -a.w,
 	};
+}
+
+sl_inline bool eq_vec4_u8(vec4_u8 a, vec4_u8 b) {
+	return (a.x == b.x) && (a.y == b.y) && (a.z == b.z) && (a.w == b.w);
+}
+sl_inline bool eq_vec4_u16(vec4_u16 a, vec4_u16 b) {
+	return (a.x == b.x) && (a.y == b.y) && (a.z == b.z) && (a.w == b.w);
+}
+sl_inline bool eq_vec4_u32(vec4_u32 a, vec4_u32 b) {
+	return (a.x == b.x) && (a.y == b.y) && (a.z == b.z) && (a.w == b.w);
+}
+sl_inline bool eq_vec4_u64(vec4_u64 a, vec4_u64 b) {
+	return (a.x == b.x) && (a.y == b.y) && (a.z == b.z) && (a.w == b.w);
+}
+sl_inline bool eq_vec4_s8(vec4_s8 a, vec4_s8 b) {
+	return (a.x == b.x) && (a.y == b.y) && (a.z == b.z) && (a.w == b.w);
+}
+sl_inline bool eq_vec4_s16(vec4_s16 a, vec4_s16 b) {
+	return (a.x == b.x) && (a.y == b.y) && (a.z == b.z) && (a.w == b.w);
+}
+sl_inline bool eq_vec4_s32(vec4_s32 a, vec4_s32 b) {
+	return (a.x == b.x) && (a.y == b.y) && (a.z == b.z) && (a.w == b.w);
+}
+sl_inline bool eq_vec4_s64(vec4_s64 a, vec4_s64 b) {
+	return (a.x == b.x) && (a.y == b.y) && (a.z == b.z) && (a.w == b.w);
+}
+sl_inline bool eq_vec4_f32(vec4_f32 a, vec4_f32 b) {
+	return (a.x == b.x) && (a.y == b.y) && (a.z == b.z) && (a.w == b.w);
+}
+sl_inline bool eq_vec4_f64(vec4_f64 a, vec4_f64 b) {
+	return (a.x == b.x) && (a.y == b.y) && (a.z == b.z) && (a.w == b.w);
 }
 
 sl_inline vec2_f32 rotate_vec2_f32(vec2_f32 v, f32 a) {
