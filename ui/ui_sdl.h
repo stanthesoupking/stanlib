c8b7377fe9431bdc64be739050562744e3b60217
Stanley Fuller <stanthesoupking@gmail.com>
2026-05-31 20:21:29 +1000

    Convert mouse position to pixels

--- ui/ui_sdl.h
+++ ui/ui_sdl.h
@@ -4,13 +4,14 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_events.h>
 
-sl_inline bool ui_event_from_sdl_event(const SDL_Event* event, UI_Event* out_event) {
+sl_inline bool ui_event_from_sdl_event(SDL_Window* window, const SDL_Event* event, UI_Event* out_event) {
 	switch (event->type) {
 		case SDL_EVENT_MOUSE_MOTION: {
+			const f32 window_scale = SDL_GetWindowDisplayScale(window);
 			*out_event = (UI_Event) {
 				.kind = UI_Event_Kind_Mouse_Move,
 				.mouse_move = {
-					.position = { event->motion.x, event->motion.y },
+					.position = { event->motion.x * window_scale, event->motion.y * window_scale },
 				},
 			};
 			return true;
